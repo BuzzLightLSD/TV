c8d14742b2200def3eab8ec61c2708d0c3443025
BuzzLightLSD <buzzlightlsd@gmail.com>
2025-02-17 19:41:12 +0530

    test.m3u

--- test.php
+++
@@ -1,7 +0,0 @@
-#EXTM3U
-#EXTINF:-1 tvg-id="897" tvg-name="Gemini TV HD" tvg-country="Telugu" tvg-logo="https://jiotv.catchup.cdn.jio.com/dare_images/images/Gemini_TV_HD.png" group-title="LOKIIPTV-Entertainment",Gemini TV HD
-http://tamashatv.live/jiotv/jiocloud.php?id=897
-#EXTINF:-1 tvg-id="" tvg-name="ZEE TELUGU" tvg-logo="http://openboxv8s.com/india/zee-telugu-in.png" ,ZEE TELUGU
-https://xtream-pstlive.vercel.app/opplex.m3u8?token=srpd3m&id=250844
-#EXTINF:-1 tvg-id="638" tvg-name="Zee Telugu" tvg-country="Telugu" tvg-logo="https://jiotv.catchup.cdn.jio.com/dare_images/images/Zee_Telugu.png" group-title="LOKIIPTV-Entertainment",Zee Telugu
-http://tamashatv.live/jiotv/jiocloud.php?id=638
